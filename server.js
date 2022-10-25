fa6343fb385fc6676aa0122f519eccb2edf8ea0f
Edward Yuabov <yuaboved@gmail.com>
2022-10-25 19:20:54 -0400

    created a get and post for calories

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
-const { Food } = require('./models')
-const { Calories } = require('./models')
+const { Food, Calories } = require('./models')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send({ msg: 'This route is being hit' })
-})
-
-//create food
-app.post('/food', async (req, res) => {
-  let createdFood = await Food.create(req.body)
-  res.json(createdFood)
-})
-
-//get foods
+//FOOD
+//Get foods
 app.get('/food', async (req, res) => {
   let getFood = await Food.find()
   res.json(getFood)
 })
-//delete food
+//Create food
+app.post('/food', async (req, res) => {
+  let createFood = await Food.create(req.body)
+  res.json(createFood)
+})
+//Delete food
 app.delete('/food/:id', async (req, res) => {
-  let deletedFood = await Food.findByIdAndDelete(req.params.id)
-  res.json(deletedFood)
+  let deleteFood = await Food.findByIdAndDelete(req.params.id)
+  res.json(deleteFood)
+})
+
+//CALORIES
+//Get calories
+app.get('/calories', async (req, res) => {
+  const getCalories = await Calories.find({})
+  res.json(getCalories)
+})
+//Create calories
+app.post('/calories', async (req, res) => {
+  let exampleCaloriesId = '635861db44c547834d6f451a'
+  const requestBody = { ...req.body, food: exampleCaloriesId }
+  const createCalories = await Calories.create(requestBody)
+  console.log(requestBody)
+  res.json(createCalories)
 })
 
 app.listen(PORT, () => {
